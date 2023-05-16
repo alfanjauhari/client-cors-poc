35c297ba8ecfd76d9bb03eafb6a32549b9c8982e
Alfan Jauhari <hi@alfanjauhari.com>
2023-05-16 14:27:04 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080', {
+        headers: {
+          Authorization: 'Basic YWRtaW46YWRtaW4=',
+        },
+      });
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      setMessage(error.message || 'Failed to fetch');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Message from fetch : {message}</h1>
+        <button onClick={fetchData}>
+          {loading ? 'Loading' : 'Click to fetch'}
+        </button>
       </header>
     </div>
   );
